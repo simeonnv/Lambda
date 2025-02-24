12b860219be590e73f87f5baa09e868edb70d311
simeonnv2 <111970903+simeonnv@users.noreply.github.com>
2025-02-24 20:28:18 +0200

    d

--- src/frontend/nuxt.config.ts
+++ src/frontend/nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@nuxtjs/color-mode',
+    '@nuxt/devtools'
+  ]
 })
